afdb192557b5a7fd0dd62b0d2ddea1d5b25d75ed
Joey Lorich <joey@cloudspace.com>
2014-06-05 14:23:52 -0400

    Add --warn option to CLI

--- lib/clint_eastwood/cli.rb
+++ lib/clint_eastwood/cli.rb
@@ -9,29 +9,44 @@ module ClintEastwood
     option 'disable-rbp', type: :boolean
     option 'disable-rubocop', type: :boolean
     option 'disable-reek', type: :boolean
+    option 'warn', type: :boolean
 
     default_task :lint
 
     desc 'lint', 'Lints the given project directory'
     def lint
+      clint = load_and_configure
+
+      # rubocop:disable Rails/Output
+      if clint.enforce
+        puts 'Project passed the linter successfully'
+      else
+        if options[:warn]
+          puts 'Linting Failed'
+        else
+          abort('Linting Failed')
+        end
+      end
+      # rubocop:enable Rails/Output
+    end
+
+    private
+
+    # Lods and configures clint
+    def load_and_configure
       path = options[:path] || File.expand_path('.')
 
       disable_rbp = options['disable-rbp'] || false
       disable_rubocop = options['disable-rubocop'] || false
       disable_reek = options['disable-reek'] || false
 
-      clint = ClintEastwood::TheEnforcer.new(
+      ClintEastwood::TheEnforcer.new(
         path,
         lint: options[:lint],
         disable_rbp: disable_rbp,
         disable_rubocop: disable_rubocop,
         disable_reek: disable_reek
       )
-
-      # rubocop:disable Rails/Output
-      abort('Linting Failed') unless clint.enforce
-      puts 'Project passed the linter successfully'
-      # rubocop:enable Rails/Output
     end
   end
 end
